8a83998d7e87a77a364bd5c55a6ef09aebac301a
dimandr0811 <dimandr@i.ua>
2020-06-24 01:56:02 +0300

    Initial commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function categories()
+    {
+        $categories = Category::get();
+        return view('categories', compact('categories'));
+    }
+
+    public function category($code)
+    {
+        $category =Category::where('code',$code)->first();
+
+        return view('category', compact('category'));
+
+    }
+
+    public function product($product = null)
+    {
+
+        return view('product', ['product' => $product]);
+    }
+}
