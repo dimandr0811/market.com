48ef9d0e43a76b97f7e1eb64e2712e159a4aea63
dimandr0811 <dimandr@i.ua>
2020-06-25 00:37:22 +0300

    add pivot

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -9,10 +9,15 @@ class BasketController extends Controller
 {
     public function basket()
     {
+        // сессия с key orderID
         $orderID = session('orderID');
+
+        //Если не нулевое, тогда в переменную записываем из таблицы ордер по ордер_айди товар. Если нулевое, то ничего
+        //не делать, соотвественно в view баскет ничего не пойдет под переменной ордер и корзина будет пустая
         if(!is_null($orderID)){
             $order = Order::findOrFail($orderID);
         }
+
         return view('basket',compact('order') );
     }
 
@@ -23,17 +28,54 @@ class BasketController extends Controller
 
     public function basketAdd($productId)
     {
+        // Создаем переменную, где key сессии orderID. Изначально null
         $orderID = session('orderID');
+        // если null, то создаем запись в бд, в таблице order через create.
         if(is_null($orderID)){
-            $order = Order::create()->id;
+            $order = Order::create();
+            // в key сессии записываем этот айди
             session(['orderID' => $order->id]);
         } else {
+            // если найден key orderID, то есть он не пустой, значит мы находим этот айди в таблице Order
             $order = Order::find($orderID);
         }
+        // Проверить или есть такой товар уже в списке
+        if ($order->products->contains($productId)){
+            //Если есть, то найти его count и увеличить на 1
+            $pivotRow = $order->products()->where('product_id', $productId)->first()->pivot;
+            $pivotRow->count++;
+            $pivotRow->update();
+        } else {
+            // затем мы в переменную, где есть таблица ордер, прикрепляем товар по его айди.
+            // Айди берется из адресной строки. Так делается каждый раз когда переходим на товар
+            $order->products()->attach($productId);
+        }
 
-        $order->products()->attach($productId);
 
 
-        return view('basket', compact('order'));
+        return redirect()->route('basket');
     }
+
+    public function basketRemove($productId)
+    {
+        $orderID = session('orderID');
+        if(is_null($orderID)){
+            return redirect()->route('basket');
+        }
+        $order = Order::find($orderID);
+        $order->products()->detach($productId);
+
+        if ($order->products->contains($productId)) {
+            $pivotRow = $order->products()->where('product_id', $productId)->first()->pivot;
+            if ($pivotRow->count < 2) {
+                $order->products()->detach($productId);
+            } else {
+                $pivotRow->count--;
+                $pivotRow->update();
+            }
+        }
+            return redirect()->route('basket');
+
+    }
+
 }
--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -18,6 +18,7 @@
                 </tr>
                 </thead>
                 <tbody>
+                @isset($order)
                 @foreach($order->products as $product)
                 <tr>
                     <td>
@@ -26,11 +27,13 @@
                             {{$product->name}}
                         </a>
                     </td>
-                    <td><span class="badge">1</span>
+                    <td><span class="badge">{{$product->pivot->count}}</span>
                         <div class="btn-group form-inline">
-                            <form action="http://internet-shop.tmweb.ru/basket/remove/1" method="POST">
-                                <button type="submit" class="btn btn-danger" href=""><span
+                            <form action="{{route('basket-remove', $product)}}" method="POST">
+                                <button type="submit" class="btn btn-danger"
+                                        href=""><span
                                         class="glyphicon glyphicon-minus" aria-hidden="true"></span></button>
+                                @csrf
                             </form>
                             <form action="{{route('basket-add', $product)}}" method="POST">
                                 <button type="submit" class="btn btn-success"
@@ -41,13 +44,14 @@
                         </div>
                     </td>
                     <td>{{$product->price}} ₽</td>
-                    <td>{{$product->price}} ₽</td>
+                    <td>{{$product->getPriceForCount()}} ₽</td>
 
                 </tr>
                 @endforeach
+                @endisset
                 <tr>
                     <td colspan="3">Общая стоимость:</td>
-                    <td>71990 ₽</td>
+                    <td>{{$order->getFullPrice()}} руб</td>
                 </tr>
                 </tbody>
             </table>
